243ca7ccfe8b48a177f85418dd38586aea5f73e5
adventl <dino_nick@hotmail.com>
2019-11-18 21:59:53 +0800

    Added Zuul and Eureka Servers, Dockerfiles, and updated yolo

--- yolo/app.py
+++ yolo/app.py
@@ -5,12 +5,24 @@ from darkflow.net.build import TFNet
 import cv2
 import numpy as np
 from urllib.request import urlopen
-from flask_cors import CORS
-import base64
+import py_eureka_client.eureka_client as eureka_client
+import logging
+
+REST_SERVER_PORT = 5000
+
+eureka_client.init_registry_client(eureka_server="http://192.168.99.100:8761/eureka",
+                                   app_name="3102-YOLO-SERVICE",
+                                   instance_port=REST_SERVER_PORT)
+print("Registered")
+
+options = {"model": "./cfg/yolo.cfg",
+           "load": "./cfg/yolo.weights", "threshold": 0.1, "gpu": 1.0}
+
+tfnet = TFNet(options)
 
 # Initialize the Flask application
 app = Flask(__name__)
-CORS(app)
+
 
 def image_to_byte_array(image: Image):
     imgByteArr = io.BytesIO()
@@ -18,23 +30,14 @@ def image_to_byte_array(image: Image):
     imgByteArr = imgByteArr.getvalue()
     return imgByteArr
 
-def image_to_base64(image: Image):
-    buffered = io.BytesIO()
-    image.save(buffered, format='JPEG')
-    imgStr = base64.b64encode(buffered.getvalue())
-    return imgStr
-
 
 # route http posts to this method
 @app.route('/api/upload', methods=['POST'])
-def main():
+def yoloImage():
     # load our input image and grab its spatial dimensions
     img = request.files["image"].read()
     img = Image.open(io.BytesIO(img))
 
-    options = {"model": "./cfg/yolo.cfg", "load": "./cfg/yolo.weights", "threshold": 0.1, "gpu": 1.0}
-
-    tfnet = TFNet(options)
     #url = request.form["image"]
     #url = "https://c8.alamy.com/comp/C8XXYP/airport-vehicles-dubai-international-airport-united-arab-emirates-C8XXYP.jpg"
 
@@ -42,26 +45,63 @@ def main():
     #resp = urlopen(url)
     #img = np.asarray(bytearray(resp.read()), dtype="uint8")
     img = np.asarray(img)
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     font = cv2.FONT_HERSHEY_SIMPLEX
 
     result = tfnet.return_predict(img)
 
-    # for row in result:
-    #     cv2.rectangle(img, tuple(row['topleft'].values()), tuple(row['bottomright'].values()), (255, 0, 0), 1)
-    #     cv2.putText(img, row['label'], tuple(row['topleft'].values()), font, 2, 255)
+    for row in result:
+        print("Row: " + str(row))
+        cv2.rectangle(img, tuple(row['topleft'].values()), tuple(
+            row['bottomright'].values()), (255, 0, 0), 1)
+        cv2.putText(img, row['label'], tuple(
+            row['topleft'].values()), font, 2, 255)
 
-    # print(type(img))
-    # np_img = Image.fromarray(img)
-    # # img_encoded = image_to_byte_array(np_img)
-    # img_encoded = image_to_base64(np_img)
-    # print(type(img_encoded))
+    print(type(img))
+    np_img = Image.fromarray(img)
+    img_encoded = image_to_byte_array(np_img)
+    print(type(img_encoded))
 
-    # return Response(response=img_encoded, status=200, mimetype="image/jpeg")
-    jsonRes = "{ \"results\": " + str(result) + " }"
-    return Response(response=jsonRes, status=200, mimetype="application/json")
+    return Response(response=img_encoded, status=200, mimetype="image/jpeg")
 
+# Converts results from prediction into JSON string
+def resultToJson(result):
+    jsonResult = "{ \"results\" : "
+    resultsStr = str(result)
+    jsonResult += resultsStr + "}"
+    print("jsonResult:\n" + jsonResult)
+    return jsonResult
+
+# route http posts to this method
+@app.route('/api/uploadTest', methods=['POST'])
+def yoloJson():
+    # load our input image and grab its spatial dimensions
+    img = request.files["image"].read()
+    img = Image.open(io.BytesIO(img))
+
+    #url = request.form["image"]
+    #url = "https://c8.alamy.com/comp/C8XXYP/airport-vehicles-dubai-international-airport-united-arab-emirates-C8XXYP.jpg"
+
+    # Read image to classify
+    #resp = urlopen(url)
+    #img = np.asarray(bytearray(resp.read()), dtype="uint8")
+    img = np.asarray(img)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    result = tfnet.return_predict(img)
+
+    response = resultToJson(result)
+    print("Response\n" + response)
+
+    return Response(response=response, status=200, mimetype="application/json")
+
+@app.route('/test', methods=['GET'])
+def testRoute():
+    print("/test reached")
+    return "FLASK TEST PASSED"
 # In case you wanna use this as backend too
 # @app.route('/')
 # def index():
@@ -80,8 +120,9 @@ def main():
 #       user = request.args.get('nm')
 #       return redirect(url_for('success',name = user))
 
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0', port=REST_SERVER_PORT)
 
 # git clone https://github.com/thtrieu/darkflow.git
 # pip install Cython
